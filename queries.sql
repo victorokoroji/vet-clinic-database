5621b11d373030715f7e8e8f6c8222eef027da2a
unknown <okorojiebube2@gmail.com>
2022-05-04 04:50:12 -0600

    Modify readme

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
