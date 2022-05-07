2d1567fbd90945f2b316aa14b9aa59fdbdc34c99
unknown <okorojiebube2@gmail.com>
2022-05-07 08:29:19 -0600

    Add visits table

--- schema.sql
+++ schema.sql
@@ -56,7 +56,15 @@ CREATE TABLE vets(
 
 -- create join table 
 CREATE TABLE specializations (
-   vet_id INT REFERENCES vets(id),
-   species_id INT REFERENCES species(species_id)
+   vet_id INT references vets(id),
+   species_id INT references species(id)
 );
+
+CREATE TABLE visits (
+   animal_id INT references animals(id),
+   vet_id INT references vets(id),
+   visit_date DATE 
+);
+
+
 
