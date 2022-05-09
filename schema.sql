0ba1fd9fe9d6a0493d6d7896be8e350b93b50f4b
unknown <okorojiebube2@gmail.com>
2022-05-09 06:51:56 -0600

    Add index to optimize record

--- schema.sql
+++ schema.sql
@@ -69,5 +69,7 @@ CREATE TABLE visits (
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
-
-
+-- Add index to optimize record
+BEGIN;
+CREATE INDEX owners_email_index ON owners(email);
+COMMIT;
